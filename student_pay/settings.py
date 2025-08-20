3297d95f9ef4bdd6471af859a163ce8a3be9a74c
carnage999-max <jamesezekiel039@gmail.com>
2025-08-20 04:44:24 +0100

    update(settings): added collectstatic setting

--- student_pay/settings.py
+++ student_pay/settings.py
@@ -151,6 +151,11 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [
+    BASE_DIR / "receipt_utils",
+]
+
+COLLECTSTATIC = config("COLLECTSTATIC", default="true", cast=bool)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
