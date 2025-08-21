6f189aff77800ed602c217ad9d9bf6a9f2c08cb1
carnage999-max <jamesezekiel039@gmail.com>
2025-08-21 09:38:40 +0100

    settings: add template dirs

--- student_pay/settings.py
+++ student_pay/settings.py
@@ -71,7 +71,7 @@ ROOT_URLCONF = "student_pay.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
